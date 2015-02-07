5587e4df0c9c3d9dc4ee82edbc1d947b37c356af
Ian Yu <ianyu_it@yahoo.com>
2015-02-07 14:03:52 -0500

    stronger

--- common_utils/hey_skeleton.py
+++ common_utils/hey_skeleton.py
@@ -4,15 +4,19 @@ import sys
 
 def Hey(name):
     if name == 'Ian':
-        print 'caution: Ian Mode'
+        print 'Caution: Ian Mode'
         name = name + '???'
     else:
-        print 'Else'
+        print 'Normal Mode:'
     name = name + '!!'
     print 'Hey', name, 'from skeleton..'
 
 def main():
-    Hey(sys.argv[1])
+    name = 'World'
+    # Get the name from the command line, using 'World' as a fallback.
+    if len(sys.argv) >= 2:
+        name = sys.argv[1]
+    Hey(name)
 
 # the standard boilerplate that calls the main() function.
 if __name__ == '__main__':

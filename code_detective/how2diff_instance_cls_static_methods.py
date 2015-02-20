2106feef275e05f0d52f40f0af6059462837092b
Ian Yu <ianyu_it@yahoo.com>
2015-02-20 13:50:58 -0500

    different method scopes

---
+++ code_detective/how2diff_instance_cls_static_methods.py
@@ -0,0 +1,17 @@
+class YourClass():
+	def do_this_with_instance(self):
+		print 'Instance method called on', self
+
+	@classmethod
+	def do_this_with_class(cls):
+		print 'Class method called on', cls
+
+	@staticmethod
+	def do_this_without_either():
+		print 'Static method called'
+
+y = YourClass()
+y.do_this_with_instance()
+y.do_this_with_class()
+y.do_this_without_either()
+
